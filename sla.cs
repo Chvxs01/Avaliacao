a8e416b3633d9bfb495d3b60a5620d6246295aed
Chvxs01 <gchaves961@gmail.com>
2026-04-27 08:35:08 -0300

    Update sla.cs
    Aula10: formatação de comentarios MarkDown

--- sla.cs
+++ sla.cs
@@ -1,1 +1,16 @@
 //SLA
+```c#
+//Exemplo de citação de código c# 
+public class Teste
+{
+    int variavel = 0;
+}
+```
+
+
+https://github.com/KyleGobel/MarkdownSharp-GithubCodeBlocks  
+https://docs.pipz.com/central-de-ajuda/learning-center/guia-basico-de-markdown#open  
+[Link direto para vídeo](https://www.markdownguide.org/getting-started/)  
+Vídeo no Youtube <https://www.youtube.com/watch?v=HJ16WEmOWTw>  
+
+
